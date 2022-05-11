025feaf6a21be2f2527ae1d7561a89ca64b67d79
Anish Dhiman <82077105+anish612@users.noreply.github.com>
2022-05-12 02:28:10 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7i-iiu_x-vs-3f$-f1=l1j1glmix+@%m!*z)z0_5bbi#5jrlk+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.83.221.50']
 
 
 # Application definition
