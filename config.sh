46ba90d2f6001f0604a8a257528342e3985a1604
Anish Dhiman <82077105+anish612@users.noreply.github.com>
2022-05-13 05:19:39 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -33,7 +33,7 @@ sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 44.202.230.216;
+    server_name 52.23.233.146;
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
         root /home/ubuntu/;
