61237c01d20325bbcca5b8dfaa0007359ec66c96
Anish Dhiman <82077105+anish612@users.noreply.github.com>
2022-05-12 22:34:06 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ ExecStart=/home/ubuntu/env/bin/gunicorn \
           --access-logfile - \
           --workers 3 \
           --bind unix:/run/gunicorn.sock \
-          nandandjango.wsgi:application
+          myproject.wsgi:application
 [Install]
 WantedBy=multi-user.target"  >  /etc/systemd/system/gunicorn.service
 
